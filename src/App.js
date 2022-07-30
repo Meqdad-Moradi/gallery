f015168114537992530f5c28b60f380bae516c48
meqdad <meqdad.moradi@outlook.com>
2022-07-30 18:17:11 +0200

    gallery fertig

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-   return <div className="App">gallery</div>;
+   const [images, setImages] = useState(null);
+
+   const getData = async () => {
+      try {
+         axios
+            .get(`https://jsonplaceholder.typicode.com/photos`)
+            .then((res) => setImages(res.data));
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   useEffect(() => {
+      getData();
+   }, []);
+   return (
+      <div className="app">
+         {images?.map((image, index) => {
+            const { albumId, id, url, title, thumbnailUrl } = image;
+
+            if (albumId >= 1 && albumId < 2) {
+               return (
+                  <div key={index} className="img-container">
+                     <div className="img-box">
+                        <img src={url} alt={id} />
+                     </div>
+                     <p>{title}</p>
+                  </div>
+               );
+            }
+         })}
+      </div>
+   );
 }
 
 export default App;
